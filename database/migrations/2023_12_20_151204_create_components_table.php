d42fcad456b821b9269715e5e70a9ae04ef36398
Anthony Whitehead <antdwhitehead@gmail.com>
2023-12-20 15:17:06 +0000

    initialize models, related classes and migrations

---
+++ database/migrations/2023_12_20_151204_create_components_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('components', function (Blueprint $table) {
+            $table->id();
+            $table->integer('component_type_id');
+            $table->integer('turbine_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('components');
+    }
+};
