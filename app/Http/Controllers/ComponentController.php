8056803a526158df352ba60af5ac7c947b8f5901
Anthony Whitehead <antdwhitehead@gmail.com>
2023-12-28 11:22:34 +0000

    do component logic

--- app/Http/Controllers/ComponentController.php
+++ app/Http/Controllers/ComponentController.php
@@ -3,11 +3,49 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\ComponentResource;
+use App\Http\Resources\GradeResource;
+use App\Models\Component;
+use App\Models\Grade;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class ComponentController extends BaseController
 {
     public function __construct(protected \App\Services\Component $componentService)
     {
         parent::__construct(service: $this->componentService, resource: ComponentResource::class);
+    }
+
+    /**
+     * Show the component resource
+     *
+     * @param Component $component
+     * @return ComponentResource
+     */
+    public function show(Component $component): ComponentResource
+    {
+        return new ComponentResource($component);
+    }
+
+    /**
+     * Get component grades
+     *
+     * @param Component $component
+     * @return AnonymousResourceCollection
+     */
+    public function getGrades(Component $component): AnonymousResourceCollection
+    {
+        return ComponentResource::collection($component->grades);
+    }
+
+    /**
+     * Get component grade
+     *
+     * @param Component $component
+     * @param Grade $grade
+     * @return GradeResource
+     */
+    public function getGrade(Component $component, Grade $grade): GradeResource
+    {
+        return $this->componentService->getGrade(componentID: $component->id, gradeID: $grade->id);
     }
 }
--- app/Services/Component.php
+++ app/Services/Component.php
@@ -2,12 +2,30 @@
 
 namespace App\Services;
 
+use App\Http\Resources\GradeResource;
 use App\Repositories\Component as ComponentRepository;
 
 class Component extends Base
 {
-    public function __construct(ComponentRepository $repository)
+    public function __construct(ComponentRepository $repository, private \App\Repositories\Grade $gradeRepository)
     {
         parent::__construct($repository);
+    }
+
+    /**
+     * Get grade
+     *
+     * @param int $componentID
+     * @param int $gradeID
+     * @return GradeResource
+     */
+    public function getGrade(int $componentID, int $gradeID): GradeResource
+    {
+        $grade = $this->gradeRepository->all(filters: [
+            'id' => $gradeID,
+            'component_id' => $componentID
+        ]);
+
+        return new GradeResource($grade);
     }
 }
