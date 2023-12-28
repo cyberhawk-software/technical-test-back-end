c4e39a2ea2bb2adc88512fd4e388331508808c67
Anthony Whitehead <antdwhitehead@gmail.com>
2023-12-28 11:34:47 +0000

    do grade type logic

--- app/Http/Controllers/GradeTypeController.php
+++ app/Http/Controllers/GradeTypeController.php
@@ -3,11 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\GradeTypeResource;
+use App\Models\GradeType;
 
 class GradeTypeController extends BaseController
 {
     public function __construct(private \App\Services\GradeType $gradeTypeService)
     {
         parent::__construct(service: $this->gradeTypeService, resource: GradeTypeResource::class);
+    }
+
+    /**
+     * Show the GradeType resource
+     *
+     * @param GradeType $gradeType
+     * @return GradeTypeResource
+     */
+    public function show(GradeType $gradeType): GradeTypeResource
+    {
+        return new GradeTypeResource($gradeType);
     }
 }
